3a9c5e5f1688113b05b8daa538aeec48c2b7b990
Abrar Ahmed <abrarahmed2185@gmail.com>
2024-07-11 17:04:11 +0500

    Starting with DataTypes

---
+++ Data_Types.py
@@ -0,0 +1,18 @@
+print("Ezzy Pzzy Lemon Squzyy.")
+
+# ===String Data type===
+
+# Literal Assignment
+firstName = "Bruce"
+lastName = "Wayne"
+
+print(type(firstName))
+print(type(firstName) == str)
+print(isinstance(firstName, str))
+
+
+# constructor function
+pizza = str("Pepperoni")
+print(type(pizza))
+print(type(pizza) == str)
+print(isinstance(pizza, str))
