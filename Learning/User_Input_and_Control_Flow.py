9cb16c205eeb35942c5b87f6bca4ec98c8f9dc7a
Abrar Ahmed <abrarahmed2185@gmail.com>
2024-07-13 18:40:06 +0500

    feat:  User_Input_and_Control_Flow ROCK PAPER SCI

--- Learning/User_Input_and_Control_Flow.py
+++ Learning/User_Input_and_Control_Flow.py
@@ -1,5 +1,43 @@
+import sys
+import random
+from enum import Enum
+
+
 print("User_Input_and_Control_Flow")
 
 
-userName = input("Please enter your Name")
-print(userName)
+class GameConfig(Enum):
+    ROCK = 1
+    PAPER = 2
+    SCISSORS = 3
+
+
+print("================")
+playerchoice = int(input("Press...\n1 for Rock,\n2 for Paper \n3 for Scissors:\n\n"))
+
+player = int(playerchoice)
+
+if playerchoice < 1 or playerchoice > 3:
+    sys.exit("You must enter 1, 2, or 3.")
+
+computerchoice = int(random.choice("123"))
+
+computer = int(computerchoice)
+
+print("================")
+print("You chose " + str(GameConfig(playerchoice)).replace("GameConfig.", "") + ".")
+print(
+    "Python chose " + str(GameConfig(computerchoice)).replace("GameConfig.", "") + "."
+)
+print("================")
+
+if playerchoice == 1 and computerchoice == 3:
+    print("🎉 You win!")
+elif playerchoice == 2 and computerchoice == 1:
+    print("🎉 You win!")
+elif playerchoice == 3 and computerchoice == 2:
+    print("🎉 You win!")
+elif playerchoice == computerchoice:
+    print("😲 Tie game!")
+else:
+    print("🐍 Python wins!")
